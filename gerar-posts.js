c13b968296136998b85b7c3069724e7dbeee1d45
Ruan Binder <105131152+binderruan@users.noreply.github.com>
2026-04-29 14:26:35 -0300

    Enhance gerar-posts.js to process news articles
    Added functionality to read markdown files and extract metadata for news articles.

--- gerar-posts.js
+++ gerar-posts.js
@@ -2,13 +2,43 @@ const fs = require("fs");
 const path = require("path");
 
 const pasta = path.join(__dirname, "noticias");
+
 const arquivos = fs.readdirSync(pasta)
-  .filter(a => a.endsWith(".md"))
-  .reverse();
+.filter(a => a.endsWith(".md"))
+.reverse();
 
 fs.writeFileSync(
-  path.join(pasta, "posts.json"),
-  JSON.stringify(arquivos)
+ path.join(pasta,"posts.json"),
+ JSON.stringify(arquivos,null,2)
 );
 
-console.log("posts.json criado com sucesso!");
+let noticias = [];
+
+arquivos.forEach(nome => {
+
+ const conteudo = fs.readFileSync(
+   path.join(pasta,nome),
+   "utf8"
+ );
+
+ const titulo = (conteudo.match(/title:\s*(.*)/i)||["","Sem título"])[1];
+ const data = (conteudo.match(/date:\s*(.*)/i)||["",""])[1];
+ const imagem = (conteudo.match(/thumbnail:\s*(.*)/i)||["","images/logo.png"])[1];
+ const categoria = (conteudo.match(/category:\s*(.*)/i)||["",""])[1];
+
+ noticias.push({
+   nome,
+   titulo: titulo.trim(),
+   data: data.trim(),
+   imagem: imagem.trim(),
+   categoria: categoria.trim()
+ });
+
+});
+
+fs.writeFileSync(
+ path.join(pasta,"noticias.json"),
+ JSON.stringify(noticias,null,2)
+);
+
+console.log("Arquivos criados!");
